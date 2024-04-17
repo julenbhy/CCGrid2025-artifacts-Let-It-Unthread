07c343888012edb92a46d0839af9516a9cd757e7
julenbhy <julenbhy@example.com>
2024-04-17 20:13:08 +0200

    Remove glibc-benches

--- wasi-threads-microbenchmarks/glibc-benches/compile.sh
+++
@@ -1,7 +0,0 @@
-#call make with TARGET=bench-pthread-lock-base
-clear
-make TARGET=bench-pthread-locks
-make TARGET=bench-pthread-mutex-lock
-make TARGET=bench-pthread-mutex-trylock
-make TARGET=bench-pthread-spin-lock
-make TARGET=bench-pthread-spin-trylock
