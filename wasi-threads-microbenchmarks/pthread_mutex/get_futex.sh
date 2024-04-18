30334db30de0372f6ca38046ae5e336b90841920
julenbhy <julenbhy@example.com>
2024-04-18 12:41:23 +0200

    Update get_futex.sh script to analyze futex calls made by a command

--- wasi-threads-microbenchmarks/pthread_mutex/get_futex.sh
+++ wasi-threads-microbenchmarks/pthread_mutex/get_futex.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script is used to analyze the ammount of futex calls made by a command.
+
 # Check if command is provided
 if [ -z "$1" ]; then
     echo "Please provide a command to analyze."
@@ -14,7 +16,6 @@ strace --follow-forks -e trace=futex -o futex_output.txt $@ 2> log.txt
 # Analyze the file for futex calls and count them
 echo -e "\nFutex from strace..."
 awk '{print $2}' futex_output.txt | sort | uniq -c | sort -nr
-
 
 
 # Same with the WASMTIME_LOG
