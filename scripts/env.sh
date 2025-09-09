5199aae01bdd9c00aa71f01739d1892f674896b4
Julen Bohoyo <73544256+julenbhy@users.noreply.github.com>
2025-09-09 16:36:23 +0200

    Create env.sh

---
+++ scripts/env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export WASI_SDK="/opt/wasi-sdk"
+export MUSL="/opt/x86_64-linux-musl-cross"
+export WASMTIME="/opt/wasmtime-v16.0.0-x86_64-linux/wasmtime"
+export IWASM="/opt/iwasm-1.3.1/iwasm"
+export WASMER="/opt/wasmer-4.2.3/bin/wasmer"
