3b8c7cc5a62c12eececd1a19a81885d107015f6f
Julen Bohoyo <73544256+julenbhy@users.noreply.github.com>
2025-09-10 11:03:12 +0200

    Update env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Versions
+export WASI_VERSION="25"
+export WASMTIME_VERSION="16.0.0"
+export IWASM_VERSION="2.1.2"
+export WASMER_VERSION="4.2.3"
+
+# Instalation paths
 export WASI_SDK="/opt/wasi-sdk"
 export MUSL="/opt/x86_64-linux-musl-cross"
-export WASMTIME="/opt/wasmtime-v16.0.0/wasmtime"
-export IWASM="/opt/iwasm-1.3.1/iwasm"
-export WASMER="/opt/wasmer-4.2.3/bin/wasmer"
+export WASMTIME="/opt/wasmtime-v${WASMTIME_VERSION}/wasmtime"
+export IWASM="/opt/iwasm-${IWASM_VERSION}/iwasm"
+export WASMER="/opt/wasmer-${WASMER_VERSION}/bin/wasmer"
