9c8ddc8f1f92c251c62dd11848f5d9569893232e
Julen Bohoyo <73544256+julenbhy@users.noreply.github.com>
2025-09-10 12:04:08 +0200

    Add CC environment variable for GCC path

--- scripts/env.sh
+++ scripts/env.sh
@@ -7,6 +7,7 @@ export IWASM_VERSION="2.1.2"
 export WASMER_VERSION="4.2.3"
 
 # Instalation paths
+export CC="/usr/bin/gcc"
 export WASI_SDK="/opt/wasi-sdk"
 export MUSL="/opt/x86_64-linux-musl-cross"
 export WASMTIME="/opt/wasmtime-v${WASMTIME_VERSION}/wasmtime"
