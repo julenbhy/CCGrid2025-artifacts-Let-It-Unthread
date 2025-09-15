f135cd214c603cc165cec1d5553895faccb71e1a
julenbhy <julenbhy@gmail.com>
2025-09-15 12:53:38 +0200

    solve path error

--- wasi-threads-microbenchmarks/concurrent-software-benchmarks/compile.sh
+++ wasi-threads-microbenchmarks/concurrent-software-benchmarks/compile.sh
@@ -11,17 +11,17 @@ for file in src/*.c; do
     if [ -f "$file" ]; then
         basename=$(basename "$file" .c)
 
-        ofile="$dir/$basename.x"
+        ofile="$path/$basename.x"
         cmd=("gcc" "-o" "$ofile" "$file" "-pthread" "-g" "-O0")
         echo "${cmd[*]}"
         "${cmd[@]}"
 
-        ofile="$dir/$basename.musl"
+        ofile="$path/$basename.musl"
         cmd=("$musl_gcc" "-o" "$ofile" "$file" "-pthread" "-g" "-O0")
         echo "${cmd[*]}"
         "${cmd[@]}"
 
-        ofile="$dir/$basename.wasm"
+        ofile="$path/$basename.wasm"
         cmd=("$WASI_SDK/bin/clang" "--target=wasm32-wasi-threads" "-Wl,--import-memory,--export-memory,--max-memory=3221225472" "-DWASM" "-o" "$ofile" "$file" "-pthread" "-g" "-O0")
         echo "${cmd[*]}"
         if ! "${cmd[@]}"; then
