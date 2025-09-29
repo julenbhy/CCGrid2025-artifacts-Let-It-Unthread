8f494caad114e5f40e1c83d8fd3f4a0811b03701
Julen Bohoyo <julenbhy@gmail.com>
2025-09-29 11:54:21 +0200

    remove warnings

--- wasi-threads-microbenchmarks/concurrent-software-benchmarks/compile.sh
+++ wasi-threads-microbenchmarks/concurrent-software-benchmarks/compile.sh
@@ -12,17 +12,17 @@ for file in src/*.c; do
         basename=$(basename "$file" .c)
 
         ofile="$path/$basename.x"
-        cmd=("gcc" "-o" "$ofile" "$file" "-pthread" "-g" "-O0")
+        cmd=("gcc" "-o" "$ofile" "$file" "-pthread" "-g" "-O0" "-w")
         echo "${cmd[*]}"
         "${cmd[@]}"
 
         ofile="$path/$basename.musl"
-        cmd=("$musl_gcc" "-o" "$ofile" "$file" "-pthread" "-g" "-O0")
+        cmd=("$musl_gcc" "-o" "$ofile" "$file" "-pthread" "-g" "-O0" "-w")
         echo "${cmd[*]}"
         "${cmd[@]}"
 
         ofile="$path/$basename.wasm"
-        cmd=("$WASI_SDK/bin/clang" "--target=wasm32-wasi-threads" "-Wl,--import-memory,--export-memory,--max-memory=3221225472" "-DWASM" "-o" "$ofile" "$file" "-pthread" "-g" "-O0")
+        cmd=("$WASI_SDK/bin/clang" "--target=wasm32-wasi-threads" "-Wl,--import-memory,--export-memory,--max-memory=3221225472" "-DWASM" "-o" "$ofile" "$file" "-pthread" "-g" "-O0" "-w" "-Wl,--no-entry" "-Wl,--export-all")
         echo "${cmd[*]}"
         if ! "${cmd[@]}"; then
             echo -e "\033[91mFailed to compile $basename\033[0m"
