06099fee80a7f59ca0180261c3c2b89357b69c37
Julen Bohoyo <73544256+julenbhy@users.noreply.github.com>
2025-10-02 18:22:05 +0200

    Add Python3 pip and numpy installation to setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,6 +10,8 @@ echo "Setting up for architecture: $ARCH"
 install_others() {
     sudo apt update
     sudo apt install -y git curl build-essential cmake m4 time multitime
+    sudo apt install -y python3-pip
+    pip3 install numpy --break-system-packages
 }
 
 install_rust() {
